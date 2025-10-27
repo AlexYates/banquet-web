6026f13c473b8dcf1a364ea3a8c2d749ea99b3ad
Yates <yates.alex@gmail.com>
2025-10-27 08:15:02 +0000

    Product Styling

--- src/components/products/ProductCard.vue
+++ src/components/products/ProductCard.vue
@@ -23,17 +23,17 @@ const formattedPrice = computed(() => {
         <CardHeader>
             <div class="aspect-square w-full bg-muted overflow-hidden rounded-lg mb-4">
                 <img :src="product.image_url" :alt="product.name"
-                    class="flex h-full w-full items-center justify-center object-cover" />
+                    class="flex h-full w-full items-center justify-center object-cover text-center" />
             </div>
             <CardTitle class="text-lg">{{ product.name }}</CardTitle>
-            <CardDescription class="text-sm h-10">{{ product.description.substring(0, 70) }}...</CardDescription>
+            <CardDescription class="text-sm h-10">{{ product.description.substring(0, 70) }}&hellip;</CardDescription>
         </CardHeader>
         <CardContent class="flex-grow">
             <p class="text-2xl font-semibold">{{ formattedPrice }}</p>
         </CardContent>
-        <CardFooter class="flex justify-between">
-            <Button variant="outline">View Details</Button>
-            <Button>Add to Cart</Button>
+        <CardFooter class="flex flex-col xl:flex-row justify-between">
+            <Button class="mb-4 w-full xl:mb-0 xl:w-auto" variant="outline">View Details</Button>
+            <Button class="w-full">Add to Cart</Button>
         </CardFooter>
     </Card>
 </template>
